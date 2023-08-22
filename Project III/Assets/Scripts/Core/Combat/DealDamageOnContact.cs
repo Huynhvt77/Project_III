24e8fa50bc0a2df4d1b7553d5b5c5cbbaac68087
Vũ Thế Huynh <94283052+Huynhvt77@users.noreply.github.com>
2023-08-22 14:34:49 +0700

    Respawning

--- Project III/Assets/Scripts/Core/Combat/DealDamageOnContact.cs
+++ Project III/Assets/Scripts/Core/Combat/DealDamageOnContact.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class DealDamageOnContact : MonoBehaviour
 {
-    [SerializeField] private int damage = 5;
+    [SerializeField] private int damage = 20;
 
     private ulong ownerClientId;
 
--- Project III/Assets/Scripts/Core/Player/TankPlayer.cs
+++ Project III/Assets/Scripts/Core/Player/TankPlayer.cs
@@ -4,16 +4,22 @@ using Unity.Netcode;
 using UnityEngine;
 using Cinemachine;
 using Unity.Collections;
+using System;
 
 public class TankPlayer : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [field: SerializeField] public Health Health { get; private set; }
 
     [Header("Settings")]
     [SerializeField] private int ownerPriority = 15;
 
     public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
+
+    public static event Action<TankPlayer> OnPlayerSpawned;
+
+    public static event Action<TankPlayer> OnPlayerDespawned;
 
     public override void OnNetworkSpawn()
     {
@@ -22,11 +28,21 @@ public class TankPlayer : NetworkBehaviour
             UserData userData =  HostSingleton.Instance.GameManager.NetworkServer.GetUserDataByClientId(OwnerClientId);
         
             PlayerName.Value = userData.userName;
+
+            OnPlayerSpawned?.Invoke(this);
         }
 
         if(IsOwner)
         {
             virtualCamera.Priority = ownerPriority;
+        }
+    }
+
+    override public void OnNetworkDespawn()
+    {
+        if(IsServer)
+        {
+            OnPlayerDespawned?.Invoke(this);
         }
     }
 }
