9d9897d94cc52388331a0791a40456dc1ce1065d
Vũ Thế Huynh <94283052+Huynhvt77@users.noreply.github.com>
2023-11-08 20:49:09 +0700

    add audio manager

---
+++ Project III/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public AudioSource[] soundEffects;
+
+    private void Awake() 
+    {
+        if (instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }    
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySFX(int sfxNumber)
+    {
+        soundEffects[sfxNumber].Stop();
+        soundEffects[sfxNumber].Play();
+    }
+}
